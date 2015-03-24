be65bb9dd78b8af495ed23ae41e9122043b2e51a
Ladeia <contato@antonioladeia.com>
2015-03-24 09:00:45 -0300

    Criando esqueleto do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+B-Duino
+
+Uma aplicaçao para para captar dados do contexto urbano e/ou natural na
+perspectiva do uso da bicicleta.
+Repositório: https://github.com/Ladeia/bduino
+
+Criado por Antonio Ladeia com base em diversos outros codigos encontrados
+na internet.
+Email: contato@antonioladeia.com
+Site: www.antonioladeia.com
+
+Este software esta licensiado sobre a GNU GENERAL PUBLIC LICENSE Version 3,
+mais detalhes encontram-se no arquivo license.txt na raiz desse pacote.
---
+++ bduino.ino
@@ -0,0 +1,85 @@
+// B-Duino
+// Uma aplicaçao para para captar dados do
+// contexto urbano e/ou natural na perspectiva
+// do uso da bicicleta.
+// Repositório: https://github.com/Ladeia/bduino
+
+// Criado por Antonio Ladeia com base em diversos
+// outros codigos encontrados na internet.
+// Email: contato@antonioladeia.com
+// Site: www.antonioladeia.com
+
+// Este software esta licensiado sobre a
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// mais detalhes encontram-se no arquivo license.txt
+// na raiz desse pacote.
+
+// Imports
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// Pegar o valor do sensor de iluminaço
+int getLightValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de temperatura
+int getTemperatureValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de umidade
+int getHumidityValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de distancia
+int getDistanceValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de latitude
+int getLatitudeValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de longitude
+int getLongitudeValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de som
+int getSoundValue()
+{
+    return 0;
+}
+
+// Pegar o valor do sensor de trepidacao
+int getShakeValue()
+{
+    return 0;
+}
+
+String createMessage()
+{
+    return "";
+}
+
+void sendMessageToSerial(String message)
+{
+
+}
